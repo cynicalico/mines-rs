1a76b19fedae0353644e35759c4c44649d995eac
cynicalico <93552084+cynicalico@users.noreply.github.com>
2024-11-19 14:40:36 -0700

    Added border, click code adjustment, moved to Bevy 0.15.0-rc3

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 #![feature(let_chains)]
 
-use std::fmt::Debug;
-use bevy::{prelude::*, sprite::Anchor, window::{PrimaryWindow, WindowResolution}};
+use bevy::{prelude::*, sprite::Anchor, window::WindowResolution};
 use rand::prelude::*;
 
-const TILE_SIZE: (u32, u32) = (16, 16);
-const SCALE: f32 = 1.0;
-const MINEFIELD_SIZE: (usize, usize) = (30, 16);
 const BACKGROUND_COLOR: Color = Color::hsv(0.0, 0.0, 0.0);
+
+const SCALE: f32 = 2.0;
+const MINEFIELD_SIZE: (usize, usize) = (10, 10);
+
+const BORDER_SIZE: (f32, f32) = (8.0, 8.0);
+const TILE_SIZE: (f32, f32) = (16.0, 16.0);
+const CONTENT_WIDTH: f32 = (2.0 * BORDER_SIZE.0) + (MINEFIELD_SIZE.0 as f32 * TILE_SIZE.0);
+const CONTENT_HEIGHT: f32 = (5.0 * BORDER_SIZE.1) + (MINEFIELD_SIZE.1 as f32 * TILE_SIZE.1);
+const MINEFIELD_OFFSET: (f32, f32) = (BORDER_SIZE.0, CONTENT_HEIGHT - (BORDER_SIZE.1 * 4.0));
 
 fn main() {
     App::new()
@@ -19,8 +24,8 @@ fn main() {
                         title: "mines-rs".into(),
                         position: WindowPosition::Centered(MonitorSelection::Primary),
                         resolution: WindowResolution::new(
-                            ((MINEFIELD_SIZE.0 as u32) * (TILE_SIZE.0)) as f32 * SCALE,
-                            ((MINEFIELD_SIZE.1 as u32) * (TILE_SIZE.1)) as f32 * SCALE,
+                            CONTENT_WIDTH * SCALE,
+                            CONTENT_HEIGHT * SCALE,
                         ),
                         resizable: false,
                         ..default()
@@ -29,37 +34,152 @@ fn main() {
                 }),
         )
         .insert_resource(ClearColor(BACKGROUND_COLOR))
-        .insert_resource(RandomizeTimer(Timer::from_seconds(
-            0.25,
-            TimerMode::Repeating,
-        )))
         .init_resource::<Minefield>()
-        .init_resource::<MinesSpriteSheet>()
+        .init_resource::<MinefieldSpriteSheet>()
+        .init_resource::<BorderSpriteSheet>()
         .add_systems(Startup, setup)
         .add_systems(Update, close_on_esc)
-        .add_systems(
-            Update,
-            (
-                handle_click,
-                (random_shuffle_sprite, update_minefield_sprites).chain(),
-            ),
-        )
+        .add_systems(Update, ((handle_click, update_minefield_sprites).chain(),))
         .run();
 }
 
 #[derive(Resource)]
-struct RandomizeTimer(Timer);
+struct MinefieldSpriteSheet(Handle<TextureAtlasLayout>);
 
-#[derive(Resource)]
-struct MinesSpriteSheet(Handle<TextureAtlasLayout>);
-
-impl FromWorld for MinesSpriteSheet {
+impl FromWorld for MinefieldSpriteSheet {
     fn from_world(world: &mut World) -> Self {
-        let texture_atlas = TextureAtlasLayout::from_grid(TILE_SIZE.into(), 4, 3, None, None);
+        let texture_atlas = TextureAtlasLayout::from_grid(
+            UVec2::new(TILE_SIZE.0 as u32, TILE_SIZE.1 as u32),
+            4,
+            3,
+            None,
+            None,
+        );
         let mut texture_atlases = world
             .get_resource_mut::<Assets<TextureAtlasLayout>>()
             .unwrap();
         Self(texture_atlases.add(texture_atlas))
+    }
+}
+
+#[repr(usize)]
+enum MinefieldSpriteIndex {
+    Hidden,
+    Flag,
+    Mine,
+    Num, // Usage: MinefieldSpriteIndex::Num.into() + 1 = 4, which is the 1 tile
+}
+
+impl From<MinefieldSpriteIndex> for usize {
+    fn from(value: MinefieldSpriteIndex) -> Self {
+        match value {
+            MinefieldSpriteIndex::Hidden => 0,
+            MinefieldSpriteIndex::Flag => 1,
+            MinefieldSpriteIndex::Mine => 2,
+            MinefieldSpriteIndex::Num => 3,
+        }
+    }
+}
+
+struct SpawnMinefieldSprite {
+    index: MinefieldSpriteIndex,
+    minefield_data: MinefieldData,
+    position: Vec2,
+}
+
+impl Command for SpawnMinefieldSprite {
+    fn apply(self, world: &mut World) {
+        let texture: Handle<Image> = world.load_asset("spritesheet.png");
+        let texture_atlas: &MinefieldSpriteSheet = world.resource();
+
+        world.spawn((
+            Sprite {
+                image: texture.clone(),
+                texture_atlas: Some(TextureAtlas {
+                    layout: texture_atlas.0.clone(),
+                    index: self.index.into(),
+                }),
+                anchor: Anchor::TopLeft,
+                ..default()
+            },
+            Transform::from_xyz(
+                MINEFIELD_OFFSET.0 + self.position.x,
+                MINEFIELD_OFFSET.1 - self.position.y,
+                0.0,
+            ),
+            self.minefield_data,
+        ));
+    }
+}
+
+#[derive(Resource)]
+struct BorderSpriteSheet(Handle<TextureAtlasLayout>);
+
+impl FromWorld for BorderSpriteSheet {
+    fn from_world(world: &mut World) -> Self {
+        let texture_atlas = TextureAtlasLayout::from_grid(
+            UVec2::new(BORDER_SIZE.0 as u32, BORDER_SIZE.1 as u32),
+            2,
+            5,
+            None,
+            Some((64, 0).into()),
+        );
+        let mut texture_atlases = world
+            .get_resource_mut::<Assets<TextureAtlasLayout>>()
+            .unwrap();
+        Self(texture_atlases.add(texture_atlas))
+    }
+}
+
+#[repr(usize)]
+enum BorderSpriteIndex {
+    Vert,
+    Hori,
+    JoinVerticalLeft,
+    JoinVerticalRight,
+    TopLeftCorner,
+    TopRightCorner,
+    BottomLeftCorner,
+    BottomRightCorner,
+}
+
+impl From<BorderSpriteIndex> for usize {
+    fn from(value: BorderSpriteIndex) -> Self {
+        match value {
+            BorderSpriteIndex::Vert => 0,
+            BorderSpriteIndex::Hori => 1,
+            BorderSpriteIndex::JoinVerticalLeft => 2,
+            BorderSpriteIndex::JoinVerticalRight => 3,
+            BorderSpriteIndex::TopLeftCorner => 4,
+            BorderSpriteIndex::TopRightCorner => 5,
+            BorderSpriteIndex::BottomLeftCorner => 6,
+            BorderSpriteIndex::BottomRightCorner => 7,
+        }
+    }
+}
+
+struct SpawnBorderSprite {
+    index: BorderSpriteIndex,
+    position: Vec2,
+}
+
+impl Command for SpawnBorderSprite {
+    fn apply(self, world: &mut World) {
+        let texture: Handle<Image> = world.load_asset("spritesheet.png");
+        let texture_atlas: &BorderSpriteSheet = world.resource();
+
+        world.spawn((
+            Sprite {
+                image: texture.clone(),
+                texture_atlas: Some(TextureAtlas {
+                    layout: texture_atlas.0.clone(),
+                    index: self.index.into(),
+                }),
+                anchor: Anchor::TopLeft,
+                ..default()
+            },
+            Transform::from_translation(self.position.extend(0.0)),
+        ));
     }
 }
 
@@ -99,75 +219,106 @@ pub fn close_on_esc(
     }
 }
 
-fn setup(
-    mut commands: Commands,
-    minefield: Res<Minefield>,
-    texture_atlas: Res<MinesSpriteSheet>,
-    window: Query<&Window, With<PrimaryWindow>>,
-    asset_server: Res<AssetServer>,
-) {
-    let window = window.single();
-
-    commands.spawn(Camera2dBundle {
-        projection: OrthographicProjection {
+fn setup(mut commands: Commands, minefield: Res<Minefield>) {
+    commands.spawn((
+        Camera2d,
+        OrthographicProjection {
             far: 1000.0,
             near: -1000.0,
             viewport_origin: Vec2::ZERO,
-            ..default()
+            scale: 1.0 / SCALE,
+            ..OrthographicProjection::default_2d()
         },
-        ..default()
+    ));
+
+    commands.queue(SpawnBorderSprite {
+        index: BorderSpriteIndex::TopLeftCorner,
+        position: (0.0, CONTENT_HEIGHT).into(),
     });
 
-    let texture = asset_server.load("spritesheet.png");
+    commands.queue(SpawnBorderSprite {
+        index: BorderSpriteIndex::TopRightCorner,
+        position: (CONTENT_WIDTH - BORDER_SIZE.0, CONTENT_HEIGHT).into(),
+    });
 
-    let center = (window.width() / 2.0, window.height() / 2.0);
-    let minefield_w = (MINEFIELD_SIZE.0 as u32 * TILE_SIZE.0) as f32 * SCALE;
-    let minefield_h = (MINEFIELD_SIZE.1 as u32 * TILE_SIZE.1) as f32 * SCALE;
-    let top_left = (
-        center.0 - (minefield_w / 2.0),
-        center.1 + (minefield_h / 2.0),
-    );
+    commands.queue(SpawnBorderSprite {
+        index: BorderSpriteIndex::BottomLeftCorner,
+        position: (0.0, BORDER_SIZE.1).into(),
+    });
+
+    commands.queue(SpawnBorderSprite {
+        index: BorderSpriteIndex::BottomRightCorner,
+        position: (CONTENT_WIDTH - BORDER_SIZE.0, BORDER_SIZE.1).into(),
+    });
+
+    // horizontal segments
+    for i in 0..2 * MINEFIELD_SIZE.0 {
+        commands.queue(SpawnBorderSprite {
+            index: BorderSpriteIndex::Hori,
+            position: (BORDER_SIZE.0 * (i as f32 + 1.0), CONTENT_HEIGHT).into(),
+        });
+
+        commands.queue(SpawnBorderSprite {
+            index: BorderSpriteIndex::Hori,
+            position: (
+                BORDER_SIZE.0 * (i as f32 + 1.0),
+                CONTENT_HEIGHT - (BORDER_SIZE.1 * 3.0),
+            )
+                .into(),
+        });
+
+        commands.queue(SpawnBorderSprite {
+            index: BorderSpriteIndex::Hori,
+            position: (BORDER_SIZE.0 * (i as f32 + 1.0), BORDER_SIZE.1).into(),
+        });
+    }
+
+    // vertical segments
+    for i in 0..(2 * MINEFIELD_SIZE.1 + 3) {
+        if i == 2 {
+            // skip the join
+            continue;
+        }
+
+        commands.queue(SpawnBorderSprite {
+            index: BorderSpriteIndex::Vert,
+            position: (0.0, CONTENT_HEIGHT - BORDER_SIZE.1 * (i as f32 + 1.0)).into(),
+        });
+
+        commands.queue(SpawnBorderSprite {
+            index: BorderSpriteIndex::Vert,
+            position: (
+                CONTENT_WIDTH - BORDER_SIZE.0,
+                CONTENT_HEIGHT - BORDER_SIZE.1 * (i as f32 + 1.0),
+            )
+                .into(),
+        });
+    }
+
+    commands.queue(SpawnBorderSprite {
+        index: BorderSpriteIndex::JoinVerticalLeft,
+        position: (0.0, CONTENT_HEIGHT - BORDER_SIZE.1 * 3.0).into(),
+    });
+
+    commands.queue(SpawnBorderSprite {
+        index: BorderSpriteIndex::JoinVerticalRight,
+        position: (
+            CONTENT_WIDTH - BORDER_SIZE.0,
+            CONTENT_HEIGHT - BORDER_SIZE.1 * 3.0,
+        )
+            .into(),
+    });
 
     for row in 0..minefield.cells.len() {
         for col in 0..minefield.cells[row].len() {
-            let x = top_left.0 + (col as u32 * TILE_SIZE.0) as f32 * SCALE;
-            let y = top_left.1 - (row as u32 * TILE_SIZE.0) as f32 * SCALE;
-
-            commands.spawn((
-                SpriteBundle {
-                    sprite: Sprite {
-                        anchor: Anchor::TopLeft,
-                        ..default()
-                    },
-                    transform: Transform {
-                        translation: (x, y, 0.0).into(),
-                        scale: Vec2::splat(SCALE).extend(0.0),
-                        ..default()
-                    },
-                    texture: texture.clone(),
-                    ..default()
-                },
-                TextureAtlas {
-                    layout: texture_atlas.0.clone(),
-                    index: minefield.cells[row][col] as usize,
-                },
-                MinefieldData {
+            commands.queue(SpawnMinefieldSprite {
+                index: MinefieldSpriteIndex::Hidden,
+                minefield_data: MinefieldData {
                     position: (row, col),
                 },
-            ));
+                position: (col as f32 * TILE_SIZE.0, row as f32 * TILE_SIZE.0).into(),
+            })
         }
-    }
-}
-
-fn random_shuffle_sprite(
-    time: Res<Time>,
-    mut timer: ResMut<RandomizeTimer>,
-    mut minefield: ResMut<Minefield>,
-) {
-    if timer.0.tick(time.delta()).just_finished() {
-        let row = thread_rng().gen_range(0..minefield.cells.len());
-        let col = thread_rng().gen_range(0..minefield.cells[row].len());
-        minefield.cells[row][col] = thread_rng().gen_range(0..12);
     }
 }
 
@@ -180,24 +331,36 @@ fn handle_click(
         && let Ok(window) = window.get_single()
         && let Some(pos) = window
             .cursor_position()
-            .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
-            .map(|ray| ray.origin.truncate())
+            .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor).ok())
+            .map(|ray| Vec2::new(ray.origin.x.floor(), ray.origin.y.ceil()))
     {
         if mouse_button_input.just_pressed(MouseButton::Left) {
-            info!("{:?}", pos);
+            if pos.x >= MINEFIELD_OFFSET.0
+                && pos.x < MINEFIELD_OFFSET.0 + (MINEFIELD_SIZE.0 as f32 * TILE_SIZE.0)
+                && pos.y <= MINEFIELD_OFFSET.1
+                && pos.y > MINEFIELD_OFFSET.1 - (MINEFIELD_SIZE.1 as f32 * TILE_SIZE.1)
+            {
+                let minefield_row =
+                    ((MINEFIELD_OFFSET.1 - pos.y) as u32 / TILE_SIZE.1 as u32) as usize;
+                let minefield_col =
+                    ((pos.x - MINEFIELD_OFFSET.0) as u32 / TILE_SIZE.0 as u32) as usize;
+                info!("{}, {}", minefield_row, minefield_col);
+            }
         }
     }
 }
 
 fn update_minefield_sprites(
-    mut sprites: Query<(&mut TextureAtlas, &MinefieldData)>,
+    mut sprites: Query<(&mut Sprite, &MinefieldData)>,
     minefield: Res<Minefield>,
 ) {
-    for (mut texture, data) in sprites.iter_mut() {
-        texture.index = if minefield.hidden[data.position.0][data.position.1] {
-            0
-        } else {
-            minefield.cells[data.position.0][data.position.1] as usize
+    for (ref mut sprite, data) in sprites.iter_mut() {
+        if let Some(texture_atlas) = &mut sprite.texture_atlas {
+            texture_atlas.index = if minefield.hidden[data.position.0][data.position.1] {
+                0
+            } else {
+                minefield.cells[data.position.0][data.position.1] as usize
+            }
         }
     }
 }
